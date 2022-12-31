cc3150574ad6e5240fa6fa08788ae234f80808b5
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-01-01 01:44:54 +0530

    setup Airflow on EKS

---
+++ data_sources.tf
@@ -0,0 +1,37 @@
+# -----------------------------------------------------------------------------------------------------
+# Data Sources
+# -----------------------------------------------------------------------------------------------------
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks_bp.eks_cluster_id
+}
+
+data "aws_availbility_zones" "available" {}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_partition" "this" {}
+
+data "aws_iam_policy_document" "airflow_s3_logs" {
+  statement {
+    sid       = ""
+    effect    = "Allow"
+    resources = ["arn:${data.aws_partition.current.partition}:s3:::${module.airflow_s3_bucket.s3_bucket_id}"]
+
+    actions = [
+      "s3:ListBucket"
+    ]
+  }
+
+  statement {
+    sid       = ""
+    effect    = "Allow"
+    resources = ["arn:${data.aws_partition.current.partition}:s3:::${module.airflow_s3_bucket.s3_bucket_id}/*"]
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject"
+    ]
+  }
+
+}
--- provider.tf
+++ provider.tf
@@ -2,11 +2,33 @@ provider "aws" {
   region = var.aws_region
 }
 
+provider "kubernetes" {
+  host                   = module.eks_bp.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_bp.eks_cluster_certificate.certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_bp.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks_bp.eks_cluster_certificate.certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
+
+provider "kubectl" {
+  apply_retry_count      = 10
+  host                   = module.eks_bp.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_bp.eks_cluster_certificate.certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+  load_config_file       = false
+}
+
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.28.0"
+      version = "~> 4.48.0"
     }
   }
 
