500652b8d4582742a1191a557fbec3d867b03ec3
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-01-02 18:09:35 +0530

    Corrected az call to data source

--- locals.tf
+++ locals.tf
@@ -3,7 +3,7 @@ locals {
   region = var.aws_region
 
   vpc_cidr                      = var.vpc_cidr
-  azs                           = slice(data.aws_availbility_zones.names, 0, 3)
+  azs                           = slice(data.aws_availability_zones.names, 0, 3)
   airflow_name                  = "airflow"
   airflow_service_account       = "airflow-webserver-sa"
   airflow_webserver_secret_name = "airflow-webserver-secret-key"
