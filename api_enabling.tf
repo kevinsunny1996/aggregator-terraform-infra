5b4682bb031fe053a48f90e2baf07de22fa2f63e
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-01-15 18:55:16 +0530

    added dependency

--- api_enabling.tf
+++ api_enabling.tf
@@ -1,5 +1,9 @@
 resource "google_project_service" "compute" {
   service = "compute.googleapis.com"
+
+  depends_on = [
+    google_project_service.container
+  ]
 }
 
 resource "google_project_service" "container" {
