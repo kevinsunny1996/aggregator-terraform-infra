a3dd9949e9c723dbca42ac37ea854fe7f56952eb
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-01-15 21:22:39 +0530

    commented out api enabling

--- api_enabling.tf
+++ api_enabling.tf
@@ -1,13 +1,13 @@
-resource "google_project_service" "compute" {
-  service = "compute.googleapis.com"
+# resource "google_project_service" "compute" {
+#   service = "compute.googleapis.com"
 
-  depends_on = [
-    google_project_service.container
-  ]
-}
+#   depends_on = [
+#     google_project_service.container
+#   ]
+# }
 
-resource "google_project_service" "container" {
-  service = "container.googleapis.com"
-}
+# resource "google_project_service" "container" {
+#   service = "container.googleapis.com"
+# }
 
 
