1f69050206dfb0035a34e328012b3c889fb97fb5
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-01-15 18:52:03 +0530

    corrected removed block

--- api_enabling.tf
+++ api_enabling.tf
@@ -1,9 +1,9 @@
+resource "google_project_service" "compute" {
+  service = "compute.googleapis.com"
+}
+
 resource "google_project_service" "container" {
   service = "container.googleapis.com"
 }
 
-resource "google_project_service" "gcp_resource_manager" {
-  project = local.id
-  service = "cloudresourcemanager.googleapis.com"
-}
 
