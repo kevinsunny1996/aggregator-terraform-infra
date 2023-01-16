9db72dba62db27f8967bdab357446e7a08e34640
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-01-16 18:59:53 +0530

    created gke cluster with nat

---
+++ nat.tf
@@ -0,0 +1,25 @@
+resource "google_compute_router_nat" "nat" {
+    name = "nat"
+    router = google_compute_router.router.name
+    region = local.region 
+
+    source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+    nat_ip_allocate_option = "MANUAL_ONLY"
+
+    subnetwork {
+      name = google_compute_subnetwork.private.id
+      source_ip_ranges_to_nat = [ "ALL_IP_RANGES" ]
+    }
+
+    nat_ips = [ google_compute_address.nat.self_link ]
+}
+
+resource "google_compute_address" "nat" {
+    name = "nat"
+    address_type = "EXTERNAL"
+    network_tier = "STANDARD"
+
+    depends_on = [
+      google_project_service.compute
+    ]  
+}
---
+++ node_pools.tf
@@ -0,0 +1,29 @@
+resource "google_service_account" "kubernetes" {
+    account_id =   "kubernetes"
+}
+
+resource "google_container_node_pool" "mlops_gke" {
+    name = "mlops_gke"
+    cluster = google_container_cluster.airflow_gke.id
+    node_count = 1
+
+    management {
+      auto_repair = true
+      auto_upgrade = true
+    }
+
+    node_config {
+      preemptible = false
+      machine_type = "e2-small"
+
+      labels = {
+        role = "mlops cluster"
+      }
+
+      service_account = google_service_account.kubernetes.email
+
+      oauth_scopes = [ "https://www.googleapis.com/auth/cloud-platform" ]
+
+    }
+}
+
