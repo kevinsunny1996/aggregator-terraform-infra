cfb2052e5cce77b78f43e0b9e464685c144bc942
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-01-16 19:05:56 +0530

    corrected allocation and cluster config blocks

--- kubernetes.tf
+++ kubernetes.tf
@@ -29,6 +29,11 @@ resource "google_container_cluster" "airflow_gke" {
   }
 
   ip_allocation_policy {
+    cluster_secondary_range_name = "k8s-pod-range"
+    services_secondary_range_name = "k8s-service-range"
+  }
+
+  private_cluster_config {
     enable_private_nodes    = true
     enable_private_endpoint = false
     master_ipv4_cidr_block  = "172.16.0.0/28"
