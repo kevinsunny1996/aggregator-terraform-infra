1f8491a868e305674a451deab5384301820849cf
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-02-03 16:21:50 +0530

    corrected formatting for data source

--- data_sources.tf
+++ data_sources.tf
@@ -3,11 +3,11 @@ data "google_client_config" "provider" {
 
 }
 
-data "google_container_cluster" "airflow_gke"{
-    name = google_container_cluster.airflow.name
-    location = local.region
+data "google_container_cluster" "airflow_gke" {
+  name     = google_container_cluster.airflow.name
+  location = local.region
 
-    depends_on = [
-      google_container_cluster.airflow
-    ]
+  depends_on = [
+    google_container_cluster.airflow
+  ]
 }
