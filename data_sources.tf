130a6ee3ace1e24d37a1519b9f36aa6e9a04dc54
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-02-03 16:21:06 +0530

    added depends_on argument

--- data_sources.tf
+++ data_sources.tf
@@ -2,3 +2,12 @@
 data "google_client_config" "provider" {
 
 }
+
+data "google_container_cluster" "airflow_gke"{
+    name = google_container_cluster.airflow.name
+    location = local.region
+
+    depends_on = [
+      google_container_cluster.airflow
+    ]
+}
--- provider.tf
+++ provider.tf
@@ -14,15 +14,15 @@ provider "helm" {
   kubernetes {
     host                   = "https://${google_container_cluster.airflow.endpoint}"
     token                  = data.google_client_config.provider.access_token
-    cluster_ca_certificate = base64decode(google_container_cluster.airflow.ca_certificate)
+    cluster_ca_certificate = base64decode(data.google_container_cluster.airflow_gke.master_auth.0.cluster_ca_certificate)
   }
 }
 
-provider "kubectl" {
-  host                   = "https://${google_container_cluster.airflow.endpoint}"
-  token                  = data.google_client_config.provider.access_token
-  cluster_ca_certificate = base64decode(google_container_cluster.airflow.ca_certificate)
-}
+# provider "kubectl" {
+#   host                   = "https://${google_container_cluster.airflow.endpoint}"
+#   token                  = data.google_client_config.provider.access_token
+#   cluster_ca_certificate = base64decode(data.google_container_cluster.airflow_gke.master_auth.0.cluster_ca_certificate)
+# }
 
 provider "random" {
 
