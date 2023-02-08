4abd45d0db6cd42e3f1e0873bf07ff4b3b6a57ae
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-02-09 03:47:27 +0530

    fixed formatting

--- apis.tf
+++ apis.tf
@@ -1,7 +1,7 @@
 resource "google_project_service" "composer_api" {
   provider = google
-  project = local.id
-  service = "composer.googleapis.com"
+  project  = local.id
+  service  = "composer.googleapis.com"
 
   disable_on_destroy = false
 }
--- service_accounts.tf
+++ service_accounts.tf
@@ -1,16 +1,16 @@
 resource "google_service_account" "airflow_composer_service_account" {
-  account_id = "airflow-composer-service-account"
+  account_id   = "airflow-composer-service-account"
   display_name = "Composer Service Account"
 }
 
 resource "google_project_iam_member" "airflow_composer_service_account" {
   project = local.id
-  member = format("serviceAccount:%s", google_service_account.airflow_composer_service_account.email)
-  role = "roles/composer.worker"
+  member  = format("serviceAccount:%s", google_service_account.airflow_composer_service_account.email)
+  role    = "roles/composer.worker"
 }
 
 resource "google_service_account_iam_member" "airflow_composer_service_account" {
   service_service_account_id = google_service_account.airflow_composer_service_account.name
-  role = "roles/composer.ServiceAgentV2Ext"
-  member = "serviceAccount:service-374508@cloudcomposer-accounts.iam.gserviceaccount.com"
+  role                       = "roles/composer.ServiceAgentV2Ext"
+  member                     = "serviceAccount:service-374508@cloudcomposer-accounts.iam.gserviceaccount.com"
 }
