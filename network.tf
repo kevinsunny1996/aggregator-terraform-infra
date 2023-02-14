b34c9c3437c5eb1ccbaac087c9ff1dff7f7b2ca1
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-02-14 13:14:56 +0530

    corrected ALL_SUBNETWORKS_ALL_IP_RANGES

--- network.tf
+++ network.tf
@@ -58,7 +58,7 @@ resource "google_compute_router_nat" "nat_gateway" {
   router                             = google_compute_router.nat_router.name
   region                             = google_compute_router.nat_router.region
   nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_IP_RANGES"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 
   log_config {
     enable = true
