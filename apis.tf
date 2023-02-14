bd5f2c7ae2cf52b101043608dc1ad4dbb217e885
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-02-14 14:30:57 +0530

    fixed formatting

--- apis.tf
+++ apis.tf
@@ -8,8 +8,8 @@ resource "google_project_service" "composer_api" {
 
 resource "google_project_service" "networking_api" {
   provider = google
-  project = local.id
-  service = "servicenetworking.googleapis.com"
+  project  = local.id
+  service  = "servicenetworking.googleapis.com"
 
   disable_on_destroy = false
 }
