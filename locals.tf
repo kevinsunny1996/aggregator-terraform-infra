e21d3974883e1bf84ca9a2136281e34ee91fabe2
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-04-14 01:07:14 +0530

    Cleanup files that are duplicates and create storage account for storing API extracts

--- locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  region    = var.gcp_region
-  gs_region = var.gcs_region
-  owner     = var.owner_email
-  name      = var.project_name
-  id        = var.project_id
-  number    = var.project_number
-}
