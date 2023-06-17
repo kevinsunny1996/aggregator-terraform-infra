ca8ffa1e1859e41a21e052759e9c013ccd0e0856
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-06-17 21:39:29 +0530

    Enabled Composer V2 API

--- terraform/apis.tf
+++ terraform/apis.tf
@@ -1,10 +1,11 @@
-# resource "google_project_service" "composer_api" {
-#   provider = google
-#   project  = local.id
-#   service  = "composer.googleapis.com"
+# Enable Composer v2 API
+resource "google_project_service" "composer_api" {
+  provider = google
+  project  = local.id
+  service  = "composer.googleapis.com"
 
-#   disable_on_destroy = false
-# }
+  disable_on_destroy = false
+}
 
 # resource "google_project_service" "networking_api" {
 #   provider = google
