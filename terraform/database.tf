b5b3ca8452cf4589bfdc66e106411555542bbf6f
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-06-17 21:57:21 +0530

    Created PG Cloud SQL instance for composer

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,8 +1,8 @@
 # Create a Cloud SQL DB instance for Cloud Composer Backend
 resource "google_sql_database_instance" "main" {
-  name             = "main-instance"
+  name             = "composer-db-backend"
   database_version = "POSTGRES_14"
-  region           = "us-central1"
+  region           = local.region
 
   settings {
     # Second-generation instance tiers are based on the machine
