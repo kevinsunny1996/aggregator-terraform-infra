acb27163e1692cc7e1f957e0cfc6f97f4a422d47
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-07-13 01:04:23 +0530

    removed dependecy

--- terraform/flyte.tf
+++ terraform/flyte.tf
@@ -16,6 +16,7 @@ resource "google_container_cluster" "flyte_cluster" {
       local_ssd_count = 0
     }
   }
+  depends_on = [google_project_service.compute_api, google_project_service.container_api]
 }
 
 resource "google_container_node_pool" "flyte_node_pool" {
