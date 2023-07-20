35c92ce806d70a2f4ebed0fe4cf814abcd1f66a0
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-07-20 15:36:57 +0530

    commented cluster and np

--- terraform/flyte.tf
+++ terraform/flyte.tf
@@ -5,64 +5,64 @@
 # 3 - Helm Release for Flyte
 # 4 - Auth client for getting CA certificate while authenticating to cluster
 ######################################################################################################################
-resource "google_container_cluster" "flyte_cluster" {
-  name                     = "flyte-cluster"
-  location                 = "${local.region}-b"
-  remove_default_node_pool = true
-  initial_node_count       = 2
+# resource "google_container_cluster" "flyte_cluster" {
+#   name                     = "flyte-cluster"
+#   location                 = "${local.region}-b"
+#   remove_default_node_pool = true
+#   initial_node_count       = 2
 
-  node_config {
-    disk_size_gb = 10
-    disk_type    = "pd-balanced"
-    ephemeral_storage_local_ssd_config {
-      local_ssd_count = 0
-    }
-  }
+#   node_config {
+#     disk_size_gb = 10
+#     disk_type    = "pd-balanced"
+#     ephemeral_storage_local_ssd_config {
+#       local_ssd_count = 0
+#     }
+#   }
 
-  release_channel {
-    channel = "REGULAR"
-  }
+#   release_channel {
+#     channel = "REGULAR"
+#   }
 
-  master_auth {
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
-  depends_on = [google_project_service.compute_api, google_project_service.container_api]
-}
+#   master_auth {
+#     client_certificate_config {
+#       issue_client_certificate = false
+#     }
+#   }
+#   depends_on = [google_project_service.compute_api, google_project_service.container_api]
+# }
 
 
-resource "google_container_node_pool" "flyte_node_pool" {
-  name       = "flyte-np"
-  location   = "${local.region}-b"
-  cluster    = google_container_cluster.flyte_cluster.name
-  node_count = 2
+# resource "google_container_node_pool" "flyte_node_pool" {
+#   name       = "flyte-np"
+#   location   = "${local.region}-b"
+#   cluster    = google_container_cluster.flyte_cluster.name
+#   node_count = 2
 
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
+#   management {
+#     auto_repair  = true
+#     auto_upgrade = true
+#   }
 
-  autoscaling {
-    min_node_count = 2
-    max_node_count = 4
-  }
+#   autoscaling {
+#     min_node_count = 2
+#     max_node_count = 4
+#   }
 
-  node_config {
-    preemptible     = true
-    machine_type    = "e2-medium"
-    service_account = google_service_account.flyte_service_account.email
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
-    # taint {
-    #   key   = "instance_type"
-    #   value = "spot"
-    #   effect = "NO_SCHEDULE"
-    # }
-  }
-  depends_on = [google_project_service.compute_api, google_project_service.container_api]
-}
+#   node_config {
+#     preemptible     = true
+#     machine_type    = "e2-medium"
+#     service_account = google_service_account.flyte_service_account.email
+#     oauth_scopes = [
+#       "https://www.googleapis.com/auth/cloud-platform",
+#     ]
+#     # taint {
+#     #   key   = "instance_type"
+#     #   value = "spot"
+#     #   effect = "NO_SCHEDULE"
+#     # }
+#   }
+#   depends_on = [google_project_service.compute_api, google_project_service.container_api]
+# }
 
 # resource "helm_release" "flyte_single_cluster" {
 #   name             = "flyte-test-setup"
