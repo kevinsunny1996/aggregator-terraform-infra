7c138cf38363f3db03b732ff3c47c6635764b0fa
Kevin Parasseril <kevinsunny1996@gmail.com>
2023-12-08 18:18:50 +0530

    commented flyte gcs bucket

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -26,25 +26,25 @@ module "gcs_api_extract" {
 #############################################################################################################
 # GCS Bucket for Flyte orchestrator Backend
 #############################################################################################################
-module "flyte_gcs_backend" {
-  source          = "terraform-google-modules/cloud-storage/google"
-  version         = "~> 3.4"
-  project_id      = local.id
-  location        = local.gs_region
-  names           = ["flyte-storage-backend"]
-  prefix          = local.name
-  force_destroy   = true
-  set_admin_roles = true
-  #   admins          = ["group:${local.owner}"]
-  versioning = {
-    terraform-state = true
-  }
-  bucket_admins = {
-    second = "user:${local.owner}"
-  }
+# module "flyte_gcs_backend" {
+#   source          = "terraform-google-modules/cloud-storage/google"
+#   version         = "~> 3.4"
+#   project_id      = local.id
+#   location        = local.gs_region
+#   names           = ["flyte-storage-backend"]
+#   prefix          = local.name
+#   force_destroy   = lookup()
+#   set_admin_roles = true
+#   #   admins          = ["group:${local.owner}"]
+#   versioning = {
+#     terraform-state = true
+#   }
+#   bucket_admins = {
+#     second = "user:${local.owner}"
+#   }
 
-  labels = {
-    project_name      = local.name
-    project_workspace = local.id
-  }
-}
+#   labels = {
+#     project_name      = local.name
+#     project_workspace = local.id
+#   }
+# }
